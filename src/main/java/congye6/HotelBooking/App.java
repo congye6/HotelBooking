7c376470fe04932af5516f70887d16b726a6f173
congye6 <244053679@qq.com>
2016-12-19 19:54:57 +0800

    config

---
+++ src/main/java/congye6/HotelBooking/App.java
@@ -0,0 +1,13 @@
+package congye6.HotelBooking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
