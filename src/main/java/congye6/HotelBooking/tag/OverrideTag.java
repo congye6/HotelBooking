558458bffb9ff5292c6e35e2af832dc863c14fd8
congye6 <244053679@qq.com>
2016-12-30 19:38:21 +0800

    override tag

---
+++ src/main/java/congye6/HotelBooking/tag/OverrideTag.java
@@ -0,0 +1,51 @@
+package congye6.HotelBooking.tag;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyContent;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * 自定义标签，用于在jsp模板中重写指定的占位内容
+ * 
+ * 基本原理：
+ *		 将block标签内容部分添加到ServletRequest的attribute属性中
+ *		 在后续override标签中再通过属性名读取出来，将其渲染到最终的页面上即可
+ * 
+ * @author 逆风之羽
+ *
+ */
+public class OverrideTag extends BodyTagSupport {
+	private static final long serialVersionUID = 5901780136314677968L;
+	//模块名的前缀
+	public static final String PREFIX = "JspTemplateBlockName_";
+	//模块名
+	private String name;
+	@Override
+	public int doStartTag() throws JspException {
+		// TODO Auto-generated method stub
+		return super.doStartTag();
+	}
+	@Override
+	public int doEndTag() throws JspException {
+		ServletRequest request = pageContext.getRequest();
+		//标签内容
+		BodyContent bodyContent = getBodyContent();
+		if(bodyContent==null){
+			request.setAttribute(PREFIX+name,"");
+		}else{
+			request.setAttribute(PREFIX+name,  bodyContent.getString());
+		}
+				
+		// TODO Auto-generated method stub
+		return super.doEndTag();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,14 +1,9 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<%@taglib uri="/congye6"  prefix="congye6"%>
 
-Welcome!
 
-</body>
-</html>
+<congye6:override name="title">模板</congye6:override>
+
+
+<jsp:include page="WEB-INF/jsp/template.jsp"></jsp:include>
